51389467f755342bc35feb51d80bddaa074aacf8
emp7yhead <artyomkropp@gmail.com>
2023-06-20 00:50:16 +0900

    add metadata for app

--- app/__init__.py
+++ app/__init__.py
@@ -1,2 +1,3 @@
+from app.books.models import Book  # noqa: F401
 from app.database import Base  # noqa: F401
 from app.users.models import User  # noqa: F401
--- app/main.py
+++ app/main.py
@@ -1,11 +1,19 @@
 from fastapi import FastAPI
 
 from app.routers import main_router
+from app.settings import settings
 
-app = FastAPI()
+app = FastAPI(
+    title=settings.app_name,
+    description=settings.description,
+    version=settings.version,
+    contact=settings.contact,
+    openapi_tags=settings.tags_metadata,
+)
+
 app.include_router(main_router)
 
 
-@app.get('/')
+@app.get('/ping')
 def ping() -> str:
     return 'pong'

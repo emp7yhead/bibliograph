49fa4c2f87972a1639f0d98d35b2e6a997808822
emp7yhead <artyomkropp@gmail.com>
2023-06-20 00:50:29 +0900

    add book and bookshelf routers

--- app/routers.py
+++ app/routers.py
@@ -1,9 +1,13 @@
 from fastapi import APIRouter
 
 from app.auth.routers import auth_router
+from app.books.routers import books_router
+from app.bookshelf.routers import bookshelf_router
 from app.users.routers import user_router
 
 main_router = APIRouter()
 
 main_router.include_router(auth_router)
 main_router.include_router(user_router)
+main_router.include_router(bookshelf_router)
+main_router.include_router(books_router)

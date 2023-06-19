f5cab9f1390d0cf2e863094a6d2a6d17e9958e01
emp7yhead <artyomkropp@gmail.com>
2023-06-20 00:50:56 +0900

    add metadata in config

--- app/settings.py
+++ app/settings.py
@@ -5,6 +5,30 @@ from pydantic import BaseSettings, PostgresDsn, validator
 
 class Settings(BaseSettings):
     app_name: str = 'bibliograph'
+    description: str = 'App for organizing book collections'
+    version: str = "0.0.1"
+    contact: dict = {
+        'name': 'Artyom Kropp',
+        'email': 'artyomkropp@gmail.com',
+    }
+    tags_metadata: list = [
+        {
+            "name": "Users",
+            "description": "Operations with users.",
+        },
+        {
+            "name": "Auth",
+            "description": "Login and registration.",
+        },
+        {
+            "name": "Books",
+            "description": "Operations with user books.",
+        },
+        {
+            "name": "Bookshelves",
+            "description": "Managing user's bookshelves.",
+        },
+    ]
     SECRET_KEY: str = 'very secret key'
     ALGORITHM: str = 'HS256'
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30

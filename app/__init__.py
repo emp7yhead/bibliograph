7706ba289e8665654f4ece4f29c7e9a7b65f89b0
emp7yhead <artyomkropp@gmail.com>
2023-06-20 15:39:12 +0900

    add models for alembic

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,6 @@
+from app.authors.models import Author  # noqa: F401
 from app.books.models import Book  # noqa: F401
+from app.bookshelf.models import Bookshelf  # noqa: F401
 from app.database import Base  # noqa: F401
+from app.sentences.models import Sentence  # noqa: F401
 from app.users.models import User  # noqa: F401

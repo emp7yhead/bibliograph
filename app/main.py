8c7a635bd304fd4b13c168afeb135ff4c770b64d
emp7yhead <artyomkropp@gmail.com>
2023-09-05 00:48:59 +0900

    add frontend template

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.routers import main_router
 from app.settings import settings
@@ -10,6 +11,21 @@ app = FastAPI(
     contact=settings.contact,
     openapi_tags=settings.tags_metadata,
 )
+
+origins = [
+    "http://localhost:3000",
+    "localhost:3000"
+]
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
 
 app.include_router(main_router)
 
